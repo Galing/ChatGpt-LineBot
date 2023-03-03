ced3722dcd70cfe1ad530cf910366c97fdff65b8
Galing <galing.hsu@gmail.com>
2023-03-03 11:22:58 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -56,7 +56,7 @@ def handle_message(event):
         return
     
     if re.match("給我圖片",event.message.text):
-        response = openai.Image.creat(
+        response = ChatGPT.openai.Image.creat(
             prompt=event.message.text.replace("給我圖片",""),
             n=1,
             size="1024x1024"
