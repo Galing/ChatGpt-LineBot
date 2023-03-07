7e4833efe8d18b0871ba47b21aeeb3cfe934a702
Galing <galing.hsu@gmail.com>
2023-03-07 14:51:28 +0800

    Update chatgpt.py

--- api/chatgpt.py
+++ api/chatgpt.py
@@ -31,7 +31,7 @@ class ChatGPT:
     def add_image(self, text):
         response = openai.Image.create(
             prompt = text,
-            n=1,
+            n=2,
             size="1024x1024"
         )
         return response
